9309b54b59a19e891792705ec9d82069e07d96c9
Bugrahan Temur <bugrahan@cliq-app.com>
2022-10-25 00:43:24 +0200

    fix stuff to work in vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue(), crossOriginIsolation()],
+  build: { target: "es2020" },
+  optimizeDeps: {
+    esbuildOptions: { target: "es2020", supported: { bigint: true } },
+  },
+});
