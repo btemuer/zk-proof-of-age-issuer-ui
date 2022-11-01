b653c942aeb28c082ba9d1e022c03d301afffb62
Bugrahan Temuer <bugrahan.temur@gmail.com>
2022-11-01 17:36:27 +0100

    Credits in the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,11 @@ import { ref } from "vue";
 <template>
   <br /><br />
   <n-space justify="center">
-    <n-h3>Zero Knowledge Proof of Age - zkApp by <b>temmuz</b> </n-h3>
+    <n-h3>Zero Knowledge Proof of Age - zkApp by <b>Buğrahan Temür</b> </n-h3>
+    <n-h2
+      >Credits to <a href="https://github.com/RaidasGrisk">ioWxss6#6308</a> for
+      the UI inspiration</n-h2
+    >
   </n-space>
   <br /><br />
 </template>
